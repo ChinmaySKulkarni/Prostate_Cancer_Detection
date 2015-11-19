b7b7cf5a8a4c35a917b6ec33840ff9d529261a18
ChinmaySKulkarni <chinmayskulkarni@gmail.com>
2015-11-19 01:00:45 -0600

    Added function call for texture features

--- src/texture_features.m
+++ src/texture_features.m
@@ -1,4 +1,6 @@
-function texture_features()
+%Retrieves texture features. Returns the F matrix from SFTA computation 
+%F -> [Number of Images X 24]
+function F = texture_features()
     [image_data,y_labels,filenames, img_x, img_y, numcolors] = load_data('../clipped_matfile_patch_data3_50x50/',1);
     image_data = image_data.';
     [total_images,~] = size(image_data);
@@ -11,31 +13,29 @@ function texture_features()
     %size(grayscale_image_data)
     
     %Extract the texture features based on SFTA.
-    F = zeros(total_images,24);
+    dims = 24;
+    F = zeros(total_images,dims);
     for i=1:total_images
-       f = sfta(reshape(grayscale_image_data(i,:,:),img_x,img_y),4);
+       f = sfta(reshape(grayscale_image_data(i,:,:),img_x,img_y),dims/6);
        F(i,:) = f;
     end
    
+    %{
     %Create the training and test splits. First 30 % is test data and
     %remaining is training data.
-    test_size = floor(0.3 * total_images);  
-    [~,dims] = size(F);
-    given_labels = zeros(total_images - test_size,1);
+    grayscale_image_data = reshape(grayscale_image_data,total_images,img_x * img_y);
+    [~,train_labels,~,test_labels] = split_test_train(grayscale_image_data,y_labels);
+    
+    [test_size,~] = size(test_labels);
     train_F = zeros(total_images - test_size,dims);
     for i=test_size + 1:total_images
-        given_labels(i - test_size) = squeeze(y_labels(i));
         train_F(i - test_size,:) = squeeze(F(i,:));
     end
-    nb = NaiveBayes.fit(train_F,given_labels);
+    nb = NaiveBayes.fit(train_F,train_labels);
     
-    actual_labels = zeros(test_size,1);    
-    test_set = zeros(test_size,img_x,img_y);
     test_F = zeros(test_size,dims);
     for i=1:test_size
-        test_set(i,:,:) = reshape(grayscale_image_data(i,:,:),img_x,img_y);
         test_F(i,:) = squeeze(F(i,:));
-        actual_labels(i) = squeeze(y_labels(i));
     end
     
     predicted_labels = zeros(test_size,1);
@@ -43,11 +43,11 @@ function texture_features()
        predicted_labels(i) = predict(nb,test_F(i,:)); 
     end    
     
-    [accuracy,precision,recall] = classifier_performance(actual_labels,predicted_labels);
+    [accuracy,precision,recall] = classifier_performance(test_labels,predicted_labels);
     accuracy
     precision
     recall
-
+    %}
    
 end
     
