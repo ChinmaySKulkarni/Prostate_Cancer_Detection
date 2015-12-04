418822045870d674e9b39cc9344722d75c3bbc37
ChinmaySKulkarni <chinmayskulkarni@gmail.com>
2015-12-04 01:35:19 -0600

    Preventing this function from overwriting images every time it is run.

--- src/modify_images.m
+++ src/modify_images.m
@@ -1,10 +1,12 @@
-function modify_images()
-    [image_data,y_labels,filenames, img_x, img_y, numcolors] = load_data('../pictures_data/complete3/',1);
+function modified_images = modify_images(write_flag)
+    [image_data,~,filenames, img_x, img_y, numcolors] = load_data('../pictures_data/complete3/',1);
     [~,total_images] = size(image_data);
     image_data = image_data.';
     image_data = reshape(image_data,total_images,img_x,img_y,numcolors);
     modified_images = zeros(total_images,img_x,img_y,numcolors);
     for i=1:total_images
         modified_images(i,:,:,:) = imadjust(squeeze(image_data(i,:,:,:)),[0.3 0.3 0.3; 1 1 1], [0.1 0.1 0.1; 1 1 1]) * 2;
-        imwrite(squeeze(modified_images(i,:,:,:)), strcat('../modified_complete3/', filenames{i}));
+        if write_flag == 1
+            imwrite(squeeze(modified_images(i,:,:,:)), strcat('../modified_complete3/', filenames{i}));
+        end
     end
