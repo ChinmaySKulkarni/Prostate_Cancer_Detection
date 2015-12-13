97b706e3d3a6b938c4ee44e31241c10c0deef6f0
ChinmaySKulkarni <chinmayskulkarni@gmail.com>
2015-12-13 17:52:53 -0600

    Corrected CHT code for epithelial cell detection

--- src/findCircles.m
+++ src/findCircles.m
@@ -1,10 +1,13 @@
-[image_data,y_labels,image_filename, imgx, imgy, numcolors] = load_data('../new_pictures_data/data4/',1);
+[image_data,y_labels,image_filename, imgx, imgy, numcolors] = load_data('../new_pictures_data/modified_data4/',1);
 numimages = size(image_data,2);
 for i=1:numimages
     image = reshape(image_data(:,i),imgx,imgy,numcolors);
-    image = rgb2gray(image);
-    [centers,radii,~]  = imfindcircles(image,[3 7],'ObjectPolarity','d');
-    clear all;
+    [centers,radii,~]  = imfindcircles(image,[2 5],'ObjectPolarity','dark','Sensitivity',0.92, 'EdgeThreshold',0.1);
+    figure
     imshow(image);
+    image_filename{i}
     viscircles(centers,radii,'EdgeColor','g');
+    if(i==10)
+        return
+    end
 end
